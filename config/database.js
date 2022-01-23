0fc8dd449ab7ee044ff10bd728553fc374b525d0
unknown <mimi.raykova@gmail.com>
2022-01-23 12:39:42 +0200

    fix schema types

--- config/database.js
+++ config/database.js
@@ -1,35 +1,15 @@
-// module.exports = ({ env }) => ({
-//   connection: {
-//     client: 'postgres',
-//     connection: {
-//       host: env('DATABASE_HOST', '127.0.0.1'),
-//       port: env.int('DATABASE_PORT', 5432),
-//       database: env('DATABASE_NAME', 'd86hdnj7mdpjeu'),
-//       user: env('DATABASE_USERNAME', ''),
-//       password: env('DATABASE_PASSWORD', '4fa65869713d083eb99c7fa7369f546c5925af4b0874aceb8523a0a4a7c19938'),
-//       ssl: env.bool('DATABASE_SSL', true),
-//     },
-//   },
-// });
 
 module.exports = ({ env }) => ({
   connection: {
     client: "postgres",
     connection: {
-      host: env(
-        "DATABASE_HOST",
-        "ec2-3-212-143-188.compute-1.amazonaws.com"
-      ),
-      port: env.int("DATABASE_PORT", 5432),
-      database: env("DATABASE_NAME", "d6jm7iihvnjq1t"),
-      user: env("DATABASE_USERNAME", "whboeugrzokvyy"),
-      password: env(
-        "DATABASE_PASSWORD",
-        "3a0cae04dc3e40580f1fd0b369185baf376a1753678a1792f0aad9a874002d5c"
-      ),
+      host: process.env.DATABASE_HOST,
+      port: process.env.DATABASE_PORT,
+      database: process.env.DATABASE_NAME,
+      user: process.env.DATABASE_USERNAME,
+      password: process.env.DATABASE_PASSWORD,
       // ssl: env.bool('DATABASE_SSL', true), //тук смених със следното, иначе излизаше грешка
       ssl: { rejectUnauthorized: false },
     }
   }
 });
-
--- config/plugins.js
+++ config/plugins.js
@@ -4,9 +4,9 @@ module.exports = ({ env }) => ({
     upload: {
         provider: 'cloudinary',
         providerOptions: {
-            cloud_name: env('CLOUDINARY_NAME', 'dszjcx6ai'),
-            api_key: env('CLOUDINARY_KEY', '522564858775833'),
-            api_secret: env('CLOUDINARY_SECRET', '9ZU7TjksBzMQoSJeKF9GtYeMf8A')
+            cloud_name: process.env.CLOUDINARY_NAME,
+            api_key: process.env.CLOUDINARY_KEY,
+            api_secret: process.env.CLOUDINARY_SECRET
         }
     }
 })
